18e3dad467b150685f6260bb68c32259da77e8c1
Troels Bay-Petersen <troelsbaypetersen@hotmail.com>
2016-08-12 17:15:23 +0200

    Got started on new front page.

--- src/main/java/clusteval/controllers/PageController.java
+++ src/main/java/clusteval/controllers/PageController.java
@@ -6,16 +6,41 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.beans.factory.annotation.*;
+
+import de.clusteval.serverclient.BackendClient;
+import java.rmi.ConnectException;
 
 @Controller
 public class PageController {
+    @Value("${port}")
+    private int port;
+
+    @Value("${clientId}")
+    private int clientId;
+
     @RequestMapping("/")
     public String homePage(Model model) {
+        boolean connectedToServer = true;
+        String absRepoPath = "";
+
+        try {
+            BackendClient backendClient = getBackendClient();
+        } catch (ConnectException e) {
+            connectedToServer = false;
+        } catch (Exception e) {}
+
+        model.addAttribute("connected", connectedToServer);
+
         return "pages/home";
     }
 
     @RequestMapping("/redirect")
     public String redirectPage(Model model) {
         return "pages/redirect";
+    }
+
+    private BackendClient getBackendClient() throws ConnectException, Exception {
+        return new BackendClient(new String[]{"-port", Integer.toString(port), "-clientId", Integer.toString(clientId)});
     }
 }
