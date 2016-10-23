e5c699e25a17882c70e0cc8ed1c53594b0054bf8
Troels Bay-Petersen <troelsbaypetersen@hotmail.com>
2016-10-23 23:38:16 +0200

    Further work on upload utilities functionality.

--- src/main/java/clusteval/controllers/UtilityController.java
+++ src/main/java/clusteval/controllers/UtilityController.java
@@ -14,23 +14,24 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.beans.factory.annotation.*;
 
 import de.clusteval.serverclient.BackendClient;
-//import de.clusteval.data.distance;
+
+import de.clusteval.data.distance.DistanceMeasure;
+import de.clusteval.data.dataset.format.DataSetFormat;
 
 import java.rmi.ConnectException;
 
-import java.util.Arrays;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.Map;
-import java.util.HashMap;
 import java.util.stream.Stream;
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Enumeration;
+import java.util.jar.*;
+import java.net.URLClassLoader;
+import java.net.URL;
 
 import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.io.FileUtils;
 
 @Controller
 public class UtilityController {
@@ -62,23 +63,66 @@ public class UtilityController {
             if (utility.getFile().isEmpty()) {
                 redirectAttributes.addFlashAttribute("failure", "Failed to store empty file");
             }
-            Path path = Paths.get(getPath() + "/supp/distanceMeasures/");
+            Path path = Paths.get(getPath() + "/suppTest/distanceMeasures/");
 
-            String extension = "";
-            try {
-                extension = utility.getFile().getOriginalFilename().split("\\.")[1];
-            } catch (Exception e) {}
+            uploadUtility(utility.getFile(), path, utility.getName(), DistanceMeasure.class);
 
-            String distanceMeasureFileName = utility.getName() + "." + extension;
-
-            Files.copy(utility.getFile().getInputStream(), path.resolve(distanceMeasureFileName));
             redirectAttributes.addFlashAttribute("success", "Distance measure uploaded successfully");
         } catch (IOException e) {
-            e.printStackTrace();
+            redirectAttributes.addFlashAttribute("failure", "Failed to upload distance measure");
+        } catch (Exception e) {
             redirectAttributes.addFlashAttribute("failure", "Failed to upload distance measure");
         }
 
         return "redirect:/utilities/upload-distance-measure";
+    }
+
+    private void uploadUtility(MultipartFile file, Path path, String name, Class utilityClass) throws IOException {
+        Files.copy(file.getInputStream(), path.resolve(file.getOriginalFilename()));
+
+        File temporaryFile = convertToFile(file);
+
+        JarFile jarFile = new JarFile(temporaryFile);
+        Enumeration<JarEntry> entries = jarFile.entries();
+
+        URL[] urls = { new URL("jar:file:" + convertToFile(file).getAbsolutePath() + "!/") };
+        URLClassLoader cl = URLClassLoader.newInstance(urls);
+
+        while (entries.hasMoreElements()) {
+            JarEntry entry = entries.nextElement();
+            if(entry.isDirectory() || !entry.getName().endsWith(".class")){
+                continue;
+            }
+
+            // -6 because of .class
+            String className = entry.getName().substring(0, entry.getName().length() - 6);
+            className = className.replace('/', '.');
+            try {
+                Class c = cl.loadClass(className);
+
+                if (utilityClass.isAssignableFrom(c)) {
+                    //The class in the jar is valid
+                } else {
+                    //The class in the jar is invalid
+                    System.err.println("Shit is fucked up!");
+                    FileUtils.deleteQuietly(new File(path.resolve(file.getOriginalFilename()).toString()));
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            //Delete temporarily copied file
+            FileUtils.deleteQuietly(temporaryFile);
+        }
+    }
+
+    private File convertToFile(MultipartFile file) throws IOException {
+        File convertedFile = new File(file.getOriginalFilename());
+        convertedFile.createNewFile();
+        FileOutputStream fos = new FileOutputStream(convertedFile);
+        fos.write(file.getBytes());
+        fos.close();
+        return convertedFile;
     }
 
     private BackendClient getBackendClient() throws ConnectException, Exception {
